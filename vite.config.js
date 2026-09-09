639c290fedc24615aff788418f48cffd72436721
Wildsens <166425291+Wildsens@users.noreply.github.com>
2026-09-09 20:01:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // <-- Додаємо це, щоб з'явився доступ по локальній мережі
-    watch: {
-      // Це залишаємо для ігнорування файлів Visual Studio
-      ignored: ['**/.vs/**']
-    }
-  }
+  base: '/Cafe/', // Додаємо шлях до репозиторію
 })
